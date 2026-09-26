9d9a3341377f85316ff251969911883014458a6e
GrassfieldK <tagusa-k@grassfield.online>
2026-09-26 13:51:14 +0900

    サイトの色を変更

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,35 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 
 const theme = createTheme({
+  primaryColor: 'sand',
+  primaryShade: { light: 6, dark: 6 },
+  autoContrast: true,
+  colors: {
+    sand: [
+      '#fff8eb',
+      '#f3e7cc',
+      '#e7d2a5',
+      '#d4b77c',
+      '#bc965d',
+      '#a67c49',
+      '#89613b',
+      '#6c4a30',
+      '#4e3425',
+      '#31231b',
+    ],
+    dark: [
+      '#f4f0e8',
+      '#e4ded3',
+      '#c9c0b1',
+      '#a79d8e',
+      '#756d61',
+      '#544e45',
+      '#39352f',
+      '#2b2925',
+      '#211f1c',
+      '#191816',
+    ],
+  },
   components: {
     Anchor: Anchor.extend({
       defaultProps: { inherit: true },
